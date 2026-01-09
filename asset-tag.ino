051880185f138e834a0f9abd5a6da7030f09188a
kaighe <kaialbertson@gmail.com>
2026-01-09 01:25:08 -0800

    changes

---
+++ asset-tag.ino
@@ -0,0 +1,67 @@
+#include <gm02sp/gm02sp.h>
+
+#define MODEM_RX 14
+#define MODEM_TX 48
+#define MODEM_RTS 21
+#define MODEM_CTS 47
+#define MODEM_RESET 45
+#define MODEM_BAUD 115200
+#define MODEM_BUFFER 128
+#define MODEM_BUFFER_THRESHOLD 122
+
+Modem modem;
+
+void modem_log(const char* message){
+  // Serial.print(">");
+  // char a = ' ';
+  // size_t counter = 0;
+  // while(a != '\0'){
+  //   a = message[counter];
+  //   counter++;
+  //   Serial.print(a, HEX);
+  //   Serial.print(" ");
+  // }
+  // Serial.println();
+  Serial.print("[MODEM] ");
+  Serial.println(message);
+}
+
+void init_modem(){
+  Serial2.setRxBufferSize(MODEM_BUFFER * 2);
+  Serial2.begin(MODEM_BAUD, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  Serial2.setPins(MODEM_RX, MODEM_TX, MODEM_CTS, MODEM_RTS);
+  Serial2.setHwFlowCtrlMode(UART_HW_FLOWCTRL_CTS_RTS, MODEM_BUFFER_THRESHOLD);
+
+  pinMode(MODEM_RESET, OUTPUT);
+  digitalWrite(MODEM_RESET, LOW);
+  delay(1000);
+  digitalWrite(MODEM_RESET, HIGH);
+
+  modem.set_logger(modem_log);
+
+  while(!modem.is_ready()){
+    while(Serial2.available()){
+      modem.encode(Serial2.read());
+    }
+  }
+  delay(100);
+}
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+
+  Serial.println("Initializing... ");
+  init_modem();
+  Serial.println("Done");
+}
+
+void loop() {
+  while(Serial2.available()){
+    modem.encode(Serial2.read());
+  }
+
+  while(Serial.available() > 0) {
+    Serial2.write(Serial.read());
+  }
+}
